5267eb4f9b39274edb35a6f4dd98c29782c76fad
kafka <hisi.91@gmail.com>
2026-04-12 13:38:12 +0000

    update confluent config

--- consumer/src/main/java/org/example/ConsumerApp.java
+++ consumer/src/main/java/org/example/ConsumerApp.java
@@ -1,33 +1,30 @@
 package org.example;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-
+import java.io.InputStream;
 import java.time.Duration;
+import java.util.List;
 import java.util.Properties;
 
-/**
- * Hello world!
- */
 public class ConsumerApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         String topicName = "vehicle-count";
-        String groupId = "iot-consumer-group";
+
         Properties configs = new Properties();
-        configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, );
-        configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, );
-        configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, );
-        configs.put(ConsumerConfig.GROUP_ID_CONFIG, );
+        try (InputStream in = ConsumerApp.class.getClassLoader().getResourceAsStream("consumer.properties")) {
+            configs.load(in);
+        }
 
-        try (KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>()) {
-            kafkaConsumer.subscribe();
+        try (KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(configs)) {
+            kafkaConsumer.subscribe(List.of(topicName));
 
-            while (true){
+            while (true) {
                 ConsumerRecords<String, String> consumerRecords = kafkaConsumer.poll(Duration.ofMillis(1000));
                 for (ConsumerRecord<String, String> record : consumerRecords) {
-                    System.out.println("Consumed message: (" + record.key() + ", " + record.value() + ")");
+                    System.out.println("Consumed message: (" + record.key() + ", " + record.value() + ")" +
+                        " --> partition=" + record.partition() + " offset=" + record.offset());
                 }
             }
         }
